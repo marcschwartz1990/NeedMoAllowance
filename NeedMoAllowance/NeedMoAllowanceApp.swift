603eb1d2e0d55076a50a5b760f35b1e2c5a9cd60
marcschwartz1990 <83684060+marcschwartz1990@users.noreply.github.com>
2024-04-14 13:30:31 -0400

    Initial Commit

---
+++ NeedMoAllowance/NeedMoAllowanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeedMoAllowanceApp.swift
+//  NeedMoAllowance
+//
+//  Created by Marc Schwartz on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NeedMoAllowanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
