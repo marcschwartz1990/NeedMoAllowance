902558a312f3d4f1e85e679e4344f4e984f5f1b6
marcschwartz1990 <83684060+marcschwartz1990@users.noreply.github.com>
2024-04-15 09:16:18 -0400

    setup preview for edit chore view

--- NeedMoAllowance/EditChoreView.swift
+++ NeedMoAllowance/EditChoreView.swift
@@ -5,14 +5,24 @@
 //  Created by Marc Schwartz on 4/15/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct EditChoreView: View {
+    @Bindable var chore: Chore
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    EditChoreView()
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Chore.self)
+        let example = Chore(name: "Clean your room", value: 0.50, isCompleted: false)
+        return EditChoreView(chore: example)
+    } catch {
+        fatalError("Failed to create model container.")
+    }
 }
